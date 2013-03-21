b1e6262fd97afe1667fb55bee240d973ba9369e9
Kenneth L <kli05@students.poly.edu>
2013-03-21 13:27:23 -0400

    Hello

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -4,6 +4,8 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
+		// $pgconnection = pg_connect('host=localhost port=5432 dbname=ams user=postgres password=root'); 
+		$multidata = ORM::factory('multidata'); 
 		$this->response->body('hello, world!');
 	}
 
