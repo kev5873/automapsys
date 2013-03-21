43cefe77a0fe8aee10c3e20108e423e1092e5d28
Kenneth L <kli05@students.poly.edu>
2013-03-21 15:00:33 -0400

    asdf

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -4,7 +4,12 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
+		// $pgconn = pg_connect('host=localhost dbname=ams user=postgres password=root'); 
+		// pg_insert($pgconn, 'multidata' , array('data'=>100)); 
 		$multidata = ORM::factory('multidata'); 
+		$data = $multidata->find('multi', '100'); 
+		// $multidata->data = 100; 
+		// $multidata->save(); 
 		$this->response->body('hello, world!');
 	}
 	
