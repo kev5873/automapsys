3ff04e6f37ce1dc549b00cb43e76e566b4691961
Kevin Ngao <techiekevin@gmail.com>
2013-03-21 15:19:41 -0400

    Ignoring log directory

--- application/logs/2013/03/21.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.'); ?>
-
-2013-03-21 13:17:25 --- EMERGENCY: ErrorException [ 1 ]: Class 'Model_multidata' not found ~ MODPATH/orm/classes/Kohana/ORM.php [ 46 ] in :
-2013-03-21 13:17:25 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
-#1 {main} in :
