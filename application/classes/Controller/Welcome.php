cf9ce6add55446f333533b6f6ceef5548990bc48
Kenneth L <kli05@students.poly.edu>
2013-03-21 15:25:12 -0400

    Git Ignore Test

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -6,9 +6,15 @@ class Controller_Welcome extends Controller {
 	{
 		// $pgconn = pg_connect('host=localhost dbname=ams user=postgres password=root'); 
 		// pg_insert($pgconn, 'multidata' , array('data'=>100)); 
-		$multidata = ORM::factory('multidata'); 
-		$data = $multidata->find('multi', '100'); 
-		// $multidata->data = 100; 
+		$multidata = ORM::factory('station'); 
+		 $data = $multidata->find('station_id', 1);
+		// echo $data->data; 
+		// $multidata->data = 200; 
+		$multidata->station_id = 1; 
+		$multidata->station_name = 'hi'; 
+		$multidata->coordinatex = 10.10; 
+		$multidata->coordinatey = 20.20; 
+		
 		// $multidata->save(); 
 		$this->response->body('hello, world!');
 	}
--- application/classes/model/station.php
+++ application/classes/model/station.php
@@ -1,7 +1,17 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Model_station extends ORM
+class Model_Station extends ORM
 {
 //	protected $_has_one = array('station_id' => array());
 //	protected $_primary_key = 'station_id';	
+
+	protected $_primary_key = 'station_id';
+	
+	protected $_table_name = 'station'; 
+	protected $_table_columns = array(
+		'station_id' => 'primary_key', 
+		'station_name' => null, 
+		'coordinatex' => null, 
+		'coordinatey' => null, 
+	); 
 }
