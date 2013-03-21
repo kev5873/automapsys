4e0f9efd7ee456909b19e1148a7cb9435d51b09c
Kevin Ngao <techiekevin@gmail.com>
2013-03-21 17:10:49 -0400

    Function bug on Feed

--- application/classes/model/feed.php
+++ application/classes/model/feed.php
@@ -3,18 +3,18 @@
 class Model_feed extends Model
 {
 
-	public downloadFeed() {
+	public function downloadFeed() {
 		// Downloads the feed from the MTA and stores it in our system, saving the file
 		// This will then be processed by the processFeed to produce an output
 		// to be inserted into the database
 	}
 
-	public processFeed($file) {
+	public function processFeed($file) {
 		// Takes the downloaded data feed from the MTA and translates it to a
 		// understandable format to insert it into the database
 	}
 
-	public insertData($data) {
+	public function insertData($data) {
 		// Insert the processed feed into the database.
 	}
 
